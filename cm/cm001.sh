c0e7590f8cf824595158700e367284682ddd12de
william coppola <admin@hacker-island.com>
2012-10-06 20:19:35 -0300

    Update cm/cm001.sh
    useful for cm001 from owasp, part of the OSWAT toolkit

--- cm/cm001.sh
+++ cm/cm001.sh
@@ -1,3 +1,11 @@
+# makes files in folder named cm001 which represents cm001 from owasp
+# expects targets to be in this format:
+#		ipaddress:portnumber
+# feed it a file from the commandline
+# ex:
+#	./cm001.sh target.lst
+#
+
 if [ ! -d ./cm001 ]; then
 
 	mkdir ./cm001
