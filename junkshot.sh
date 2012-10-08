efc8402e003e71f8d19c4c8434875d367460c32e
william coppola <admin@hacker-island.com>
2012-10-08 03:20:56 -0300

    Update junkshot.sh
    used to test bad verbs and unexpected http options

--- junkshot.sh
+++ junkshot.sh
@@ -1,18 +1,35 @@
 #
+
+# Use your own list of https web targets and it will test some vaerbs for http protocol to include junk
+# Which will reply to verbs which have no meaning in the protocol which could indicate verb tampering
 #
-# the lonely `sed -r "s/(st((o)n(er))/c\3d\4/g"` seems to free his mind @ night @@@NIGHT
+#
 #
 
 
-listed = ["JUNK / HTTP/1.0", "JUNK / HTTP/1.1", "JUNK / HTTP/2.0", "JUNK / HTTP/3.0"
-]
 
+if [ ! -f verbage.txt ]; then
+ cat << EOF > verbage.txt 
+GET / HTTP/1.0
+JUNK / HTTP/1.0
+JUNK / HTTP/1.1
+JUNK / HTTP/3.0
+HEAD / HTTP/1.0
+HEAD / HTTP/1.1
+HEAD / HTTP/2.0
+HEAD / HTTP/3.0
+OPTIONS / HTTP/1.0
+EOF
+fi
 echo ""
-for x in $(cat ./httsponly); do 
- for words in $listed; do 
-  echo $words| ncat --ssl  -w 1 $x 443 -i 1 2> /dev/null |\ 
-  grep -E "^HTTP(.*)OK" | \
-  sed -r "s/(.*)/`echo $x` - \1/"g
+for x in $(cat ./httpsonly); do 
+ #echo $x
+ for verbs in $(cat verbage.txt); do 
+  #echo -e $verbs
+  request=$(echo -e `echo $verbs | tr -s "\n" "," | sed -r "s/((.*)\.[0-9]),/\1\n/g" | tr -s "," " "` | ncat --ssl  -w 1 $x 443 -i 1 &)
+ case 
+
+  #|  sed -r "s/(.*)/"`echo $x && echo  && echo $verbs`" - \1/g" & 
  done
 done
 echo ""
