c2ccc740ef96582a49bd293f77eb167275427d4b
william coppola <admin@hacker-island.com>
2012-10-08 01:54:26 -0300

    Update junkshot.sh

--- junkshot.sh
+++ junkshot.sh
@@ -1,1 +1,18 @@
-echo ""; for x in $(cat ./httsponly); do echo -e "GET / JUNK/1.1\n\n\n" | ncat --ssl  -w 1 $x 443 -i 1 2> /dev/null| grep -E "^HTTP(.*)OK" | sed -r "s/(.*)/`echo $x` - \1/"g; done; echo ""
+#
+#
+# the lonely `sed -r "s/(st((o)n(er))/c\3d\4/g"` seems to free his mind @ night @@@NIGHT
+#
+
+
+listed = ["JUNK / HTTP/1.0", "JUNK / HTTP/1.1", "JUNK / HTTP/2.0", "JUNK / HTTP/3.0"
+]
+
+echo ""
+for x in $(cat ./httsponly); do 
+ for words in $listed; do 
+  echo $words| ncat --ssl  -w 1 $x 443 -i 1 2> /dev/null |\ 
+  grep -E "^HTTP(.*)OK" | \
+  sed -r "s/(.*)/`echo $x` - \1/"g
+ done
+done
+echo ""
