66a3268bed9d23626a88f09b2a882c6d0cdb7167
william coppola <admin@hacker-island.com>
2012-10-13 14:16:16 -0300

    Update phase1/kickoff.sh
    made some corrections to spelling

--- phase1/kickoff.sh
+++ phase1/kickoff.sh
@@ -17,13 +17,14 @@ E_BADARGS=66
 if [ $# -ne $EXPECTED_ARGS ]
 then
 clear
-  echo -e "\nWritten by William SubINacls Coppola\n"
+  echo -e "\n`basename $0`: Written by William SubINacls Coppola\n"
   echo "This tool requires 3 arugments:"
   echo -e "\t1) Target ip list"
   echo -e "\t2) Clients Name"
   echo -e "\t3) Timing for the port scanning\n"
-  echo -e "Usage: base -e `basename $0` ~/somedir/target_ips.txt clients_name [1-5]:timing\n"
-  echo -e "Example: base -e `basename $0` /home/userX/targets.txt ACME_PHARM 4\n\n"
+  echo -e "Usage: bash -e `basename $0` ~/somedir/target_ips.txt clients_name [1-5]:timing"
+  echo -e "Example: bash -e `basename $0` /home/userX/targets.txt ACME_PHARM 4"
+Example: bash -e `basename $0` /home/userX/targets.txt ACME_PHARM 4\n\n"
   exit $E_BADARGS
 else
  nmap -sTV -vvv -n -T `echo $3` -p- -P0 -A -oA `echo $2` -iL `echo $1`
