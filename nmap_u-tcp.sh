7b864d3cc278636fd2c55b8428b34e65010e9b4b
william coppola <admin@hacker-island.com>
2013-04-15 13:26:40 -0500

    nmap script for kickoff disco on alll tcp and select udp ports
    made a kickoff disco nmap for select UDP ports and ALL TCP

---
+++ nmap_u-tcp.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# feed namp a list of ports and IP/Hostnames for testing
+# your port list should look like this U:123,U140-150,T:1,T:2-
+# saved to a file to be concatinated into the script command
+
+# help information
+if [[ $1 == "-h" || $1 == "h" || $1 == "help" || $1 == "-help" || $1 == "--help" || $1 == "--h" || $1 == "-H" || $1 == "-HELP" || $1 == "--HELP" || $1 == "H" || $1 == "HELP" ]];then
+  echo -e "\n[-] Cmd Usage:"
+  echo -e "\t./nmap_me.sh ./your_port_list.txt ./your_host_list.txt\n"
+  exit
+fi
+
+# check for proper parameters for application, only 2 required
+if [[ ! -z $3 ]];then
+  echo -e "\n[-] Cmd Usage:"
+  echo -e "\t./nmap_me.sh ./your_port_list.txt ./your_host_list.txt\n"
+  exit
+fi
+
+# check for portlist
+if [[ -z $1 ]]; then
+  echo -e "\n\t[*] Please supply your portlist to be used\n"
+  exit
+fi
+
+# check for port and host list and continue if found, else kill yourself...
+if [[ ! -z $1  &&  -z $2 ]]; then
+  echo -e "\n\t[*] Please supply your hostlist to be used\n"
+  exit
+fi
+
+# actual command to construct and execute
+if [[ ! -z $1  &&  ! -z $2 ]]; then
+  if [[ -f $1  &&  -f $2 ]]; then
+    nmap -sCV -n -vvv -p `cat $1` -A -O -iL $2 -oA NMAP_UDP_SELECT_TCP-ALL_`pwd | cut -d"/" -f3`
+  fi
+else
+  echo -e "\t[-] Application terminated with unknown error\n"
+  exit
+fi
+
+# additional statements may follow
